3e50c2522f85b54b29acb111a3b114fece8b2a5f
Evgeniy Albrant <skizee@yandex.ru>
2022-02-13 22:05:34 +0700

    fixed problems from reviewer

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,6 @@ from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
 from posts.models import Group, Post, User
-
 from .permissions import OwnerOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer, UserSerializer)
